95feb9e18ba901dd1cc1c0687ceb558a4cbf490c
Thien Nguyen <nhoxpiin2306@gmail.com>
2023-06-28 20:35:37 +0700

    chore: update code

---
+++ ManagementStore/Form/Camera/HomeCamera.Designer.cs
@@ -0,0 +1,63 @@
+﻿
+namespace ManagementStore.Form.Camera
+{
+    partial class HomeCamera
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBoxHome = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxHome)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBoxHome
+            // 
+            this.pictureBoxHome.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBoxHome.Location = new System.Drawing.Point(0, 0);
+            this.pictureBoxHome.Name = "pictureBoxHome";
+            this.pictureBoxHome.Size = new System.Drawing.Size(706, 330);
+            this.pictureBoxHome.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBoxHome.TabIndex = 0;
+            this.pictureBoxHome.TabStop = false;
+            // 
+            // HomeCamera
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.pictureBoxHome);
+            this.Name = "HomeCamera";
+            this.Size = new System.Drawing.Size(706, 330);
+            this.Click += new System.EventHandler(this.HomeCamera_Click);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxHome)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxHome;
+    }
+}
